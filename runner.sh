82f33af0176abdaf1e0932018049179c4f75e740
Tan Nguyen <tannp27@gmail.com>
2026-03-19 20:13:15 +0700

    Fix parameter expansion in runner.sh for cache handling

--- runner.sh
+++ runner.sh
@@ -44,7 +44,7 @@ case "$1" in
     ;;
 
   all | a)
-    if [ "$2" = "--cache" ]; then
+    if [ "${2:-}" = "--cache" ]; then
       home_sync all
       build_fe2 cache
       worker "$HOME_FE2_DIR"
